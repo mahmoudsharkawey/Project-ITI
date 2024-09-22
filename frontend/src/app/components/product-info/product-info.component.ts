61bab61545b6fe651538f143c58ea2593f1594c0
mahmoudsharkawey <ma7moudelsharkawey@gmail.com>
2024-09-22 20:10:34 +0300

    init the project

---
+++ frontend/src/app/components/product-info/product-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-info.component.html',
+  styleUrl: './product-info.component.css'
+})
+export class ProductInfoComponent {
+
+}
