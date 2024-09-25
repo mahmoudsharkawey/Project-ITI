250bb9aeb8d05de1e1b2e0e1438dbd1edfb8a87d
YoussefMassoud <youssefbusiness133@gmail.com>
2024-09-25 10:47:53 +0300

    make product style

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ProductInfoComponent } from './components/product-info/product-info.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent,HeroComponent,FooterComponent],
+  imports: [RouterOutlet,NavBarComponent,HeroComponent,FooterComponent,ProductInfoComponent,ProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
