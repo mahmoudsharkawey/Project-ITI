078ac2ca8d2d24236bdb5b6db900b4f62074a7f4
YoussefMassoud <youssefbusiness133@gmail.com>
2024-09-27 14:09:13 +0300

    edit in style cart and make hero hidden in product ingo and cart

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,18 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductInfoComponent } from './components/product-info/product-info.component';
 import { ProductsComponent } from './components/products/products.component';
+import { filter } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent,HeroComponent,FooterComponent,ProductInfoComponent,ProductsComponent,],
+  imports: [CommonModule,RouterOutlet,NavBarComponent,HeroComponent,FooterComponent,ProductInfoComponent,ProductsComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
+  hideHero: boolean = false;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        // Check if the current route is cart or product-info
+        this.hideHero = event.url.includes('/cart') || event.url.includes('/product');
+        console.log('Current URL:', event.url, 'Hide Hero:', this.hideHero); // Debugging line
+      }); 
+  }
 }
